cb87fd0efed15adf0822b9079e4a1a5a06826e77
rv373 <rv373@njit.edu>
2018-10-06 23:15:48 -0400

    Task: Added Composer.json file

--- public/index.php
+++ public/index.php
@@ -1,8 +1,83 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: rv373
+ * User: Riyaz Varangal
  * Date: 10/6/18
- * Time: 9:37 PM
+ * Time: 10:24 PM
  */
-echo 'test';
+
+main::start("FL_insurance_sample.csv");
+class main{
+    static public function start($filename)
+    {
+        $records=csv::getRecords($filename);
+        print_r($records);
+        foreach($records as $record){
+
+            //print_r($record);
+            $array = $record->returnArray();
+            print_r($array);
+        }
+        //$record= recordFactory::create();
+        //
+
+    }
+}
+
+class csv{
+    static public function getRecords($filename){
+        $file=fopen($filename,"r");
+        $fieldNames=array();
+        $count=0;
+        while(! feof($file))
+        {
+            $values= fgetcsv($file);
+            if($count == 0)
+            {
+                $fieldNames[]=$values;
+            }
+            else{
+                $records[]=recordFactory::create($fieldNames, $values);
+            }
+            $count++;
+
+        }
+        fclose($file);
+        return $records;
+
+    }
+}
+class record{
+    public function _construct(Array $fieldNames=null,$values =  null)
+    {
+
+        $record= array_combine($fieldNames,$values);
+        print_r($record);
+        //$record= (object) $record;
+        foreach($record as $property => $value){
+            $this->createProperty($property, $value);
+        }
+
+    }
+    public function returnArray()
+    {
+        $array = (array) $this;
+        return $array;
+
+    }
+    public function createProperty($name='First',$value='Krutika')
+    {
+        $this->{$name} = $value;
+        //return $html;
+    }
+
+}
+class recordFactory{
+
+    public static function create(Array $fieldNames=null,Array $values = null){
+
+        $record=new record($fieldNames,$values);
+        return $record;
+    }
+
+}
