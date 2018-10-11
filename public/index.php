5b779106849748308e43089a9eebf71ac3b5cf8c
rv373 <rv373@njit.edu>
2018-10-11 09:12:02 -0400

    Fix: bootstrap table fix

--- public/index.php
+++ public/index.php
@@ -83,9 +83,9 @@ class html {
         $html .= '<title>PHP MiniProj: Creating Table from CSV</title>';
         $html .= '<meta charset="utf-8">';
         $html .= '<meta name="viewport" content="width=device-width, initial-scale=1">';
-        $html .= '<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">';
-        $html .= '<script src="http://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>';
-        $html .= '<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>';
+        $html .= '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">';
+        $html .= '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>';
+        $html .= '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>';
         $html .= '</head>';
         $html .= '<body>';
         $html .= '<div class="container">';
